ff9d04217d691f6242f63dd93947ce2493728689
cindycheng061 <cindycheng61@gmail.com>
2023-05-20 10:30:20 +1000

    add walk

--- project1.js
+++ project1.js
@@ -1,9 +1,10 @@
-console.log("Hello");
-
-const readHeader = document.getElementById("read");
-
 const imageBox = document.getElementById("image-box");
 
+const readHeader = document.getElementById("read");
 readHeader.addEventListener("mouseover", () => {
   imageBox.style.backgroundImage = `url("image/Read.jpg")`;
+});
+const walkheader = document.getElementById("walk");
+walkheader.addEventListener("mouseover", () => {
+  imageBox.style.backgroundImage = `url("image/Walk.jpg")`;
 });
